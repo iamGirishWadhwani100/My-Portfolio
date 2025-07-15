18989ae5a9153e85ed5161997f541d5555dd05c7
Girish Wadhwani <girishwadhwani1000@gmail.com>
2025-07-16 03:22:51 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,21 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/iamgirishwadhwani_100?igsh=cGhnemx6dnhmdTly"
+        href="https://www.instagram.com/huzaifahmedz/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://x.com/GirishWad20716" target="_blank" rel="noopener noreferrer">
+      <a href="https://x.com/GirishWad20716?t=kc7z-OLJhdHYzYU6RTr12w&s=08" target="_blank" rel="noopener noreferrer">
         <FaXTwitter size={24} />
       </a>
       <a
-        href="https://www.linkedin.com/in/girish-wadhwani-5564a2227/"
+        href="https://www.linkedin.com/in/girish-wadhwani-5564a2227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaLinkedin size={24} />
-      </a>
-      <a
-        href="https://medium.com/@girishwadhwani100"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaMedium size={24} />
       </a>
     </footer>
   );
